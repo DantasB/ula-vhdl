5442859e44e25d41706b515368b470345b508b12
Bruno Dantas de Paiva <bruno.d.paiva@hotmail.com>
2019-05-06 16:22:45 -0300

    Create ULA.vhd

---
+++ ULA.vhd
@@ -0,0 +1,48 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    16:45:07 04/16/2019 
+-- Design Name: 
+-- Module Name:    ULA - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity ULA is
+    Port ( A : in  STD_LOGIC_VECTOR (3 downto 0);
+           B : in  STD_LOGIC_VECTOR (3 downto 0);
+           Operacao : in  STD_LOGIC_VECTOR (3 downto 0);
+           Z : out  STD_LOGIC_VECTOR (3 downto 0));
+end ULA;
+
+architecture Behavioral of ULA is
+
+COMPONENT somapain4
+    Port ( x : in  STD_LOGIC_VECTOR (3 downto 0);
+           y : in  STD_LOGIC_VECTOR (3 downto 0);
+           cin : in  STD_LOGIC;
+           cout : out  STD_LOGIC;
+           saida : out  STD_LOGIC_VECTOR (3 downto 0));
+END COMPONENT somapain4;
+
