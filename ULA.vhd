848f91641806ed499e7e29a63ea537d8ef8e4e1a
Bruno Dantas de Paiva <bruno.d.paiva@hotmail.com>
2019-05-06 17:15:41 -0300

    Update ULA.vhd

--- ULA.vhd
+++ ULA.vhd
@@ -88,4 +88,56 @@ COMPONENT incrementa1 is
            saida : out  STD_LOGIC_VECTOR (3 downto 0));
 end COMPONENT incrementa1;
 
-signal Not_A, Compl_A, A_mais_B, A_mais_1, A_minus_B, A_and_B, A_xor_B, A_or_B : STD_LOGIC_VECTOR (3 downto 0);
+signal Not_A, Compl_A, A_mais_B, A_mais_1, A_menos_B, A_and_B, A_xor_B, A_or_B : STD_LOGIC_VECTOR (3 downto 0);
+
+begin
+
+	-- Declarando os componentes
+	U0: Inversor port map (A, Not_A); -- Para operação 0
+	U1: Complementa port map (A, Compl_A); -- Para operação 1
+	U2: somapain4 port map (A, B, '0', A_mais_B); -- Para operaçã9o 2
+	U3: incrementa1 port map (A, A_mais_1); -- Para operação 3
+	U4: subtratain4 port map (A, B, '0', A_menos_B); -- Para operação 4
+	U5: Modulo_And port map (A, B, A_and_B); -- Para operação 5
+	U6: Modulo_Xor port map (A, B, A_xor_B); -- Para operação 6
+	U7: Modulo_Or port map (A, B, A_or_B); -- Para operação 7
+	
+	process (Operacao, Not_A, Compl_A, A_mais_B, A_mais_1, A_menos_B, A_and_B, A_xor_B, A_or_B)
+	begin
+		case Operacao is
+			when "0000" =>
+				Z <= A_and_B;
+			when "1000" =>
+				Z <= A_and_B;
+			when "0001" =>
+				Z <= A_xor_B;
+			when "1001" =>
+				Z <= A_xor_B;
+			when "0010" =>
+				Z <= A_mais_1;
+			when "1010" =>
+				Z <= A_mais_1;
+			when "0011" =>
+				Z <= A_menos_B;
+			when "1011" =>
+				Z <= A_menos_B;
+			when "0100" =>
+				Z <= A_or_B;
+			when "1100" =>
+				Z <= A_or_B;
+			when "0101" =>
+				Z <= A_mais_B;
+			when "1101" =>
+				Z <= A_mais_B;
+			when "0110" =>
+				Z <= Compl_A;
+			when "1110" =>
+				Z <= Compl_A;
+			when "0111" =>
+				Z <= Not_A;
+			when "1111" =>
+				Z <= Not_A;
+		end case;
+	end process;
+
+end Behavioral;
