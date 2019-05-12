c2fe571a421efb8749572d363428bae3d869e1de
Bruno Dantas de Paiva <bruno.d.paiva@hotmail.com>
2019-05-12 15:44:11 -0300

    Update contapain.vhd

--- contapain.vhd
+++ contapain.vhd
@@ -38,11 +38,11 @@ entity contapain is
            Led2 : out  STD_LOGIC;
            Led3 : out  STD_LOGIC;
            Led4 : out  STD_LOGIC;
-			  Led5 : out  STD_LOGIC;
+	   Led5 : out  STD_LOGIC;
            Led6 : out  STD_LOGIC;
            Led7 : out  STD_LOGIC;
            Led8 : out  STD_LOGIC;
-			  clk: in std_logic
+		   clk: in std_logic
 			  );
 end contapain;
 
@@ -61,7 +61,8 @@ end component ULA;
 signal clock : STD_LOGIC_VECTOR (7 downto 0);
 signal vetor1 : STD_LOGIC_VECTOR (3 downto 0);
 signal vetor2 : STD_LOGIC_VECTOR (3 downto 0);
-signal result : STD_LOGIC_Vector (3 downto 0);
+signal result : STD_LOGIC_VECTOR (3 downto 0);
+signal redutor : STD_LOGIC_VECTOR (‭27 downto 0)‬;
 signal fzero : STD_LOGIC;
 signal fsinal : STD_LOGIC;
 signal fover : STD_LOGIC;
@@ -69,6 +70,7 @@ signal fcout : STD_LOGIC;
 signal oparcione: STD_LOGIC_VECTOR (3 downto 0);
 signal parte: STD_LOGIC;
 begin
+redutor <= "0000000000000000000000000000";
 clock <= "00000000";
 vetor1<= "0000";
 vetor2<= "0000";
@@ -80,43 +82,48 @@ oparcione(3) <= alavanca4;
 	ulala: ULA port map (vetor1, vetor2, oparcione, result, fzero, fsinal, fover, fcout);
 	bigode: process(clk)
 	begin
-	
-		case parte is
-		
-			when '0'=>
-			
-		--if parte 0
-				led1<= clock(0);
-				led2<= clock(1);
-				led3<= clock(2);
-				led4<= clock(3);
-				led5<= clock(4);
-				led6<= clock(5);
-				led7<= clock(6);
-				led8<= clock(7);
-			when '1'=>
-		--if parte 1
-				vetor1(0) <= clock(0);
-				vetor1(1) <= clock(1);
-				vetor1(2) <= clock(2);
-				vetor1(3) <= clock(3);
-				vetor2(0) <= clock(4);
-				vetor2(1) <= clock(5);
-				vetor2(2) <= clock(6);
-				vetor2(3) <= clock(7);
-				led1<= result(0);
-				led2<= result(1);
-				led3<= result(2);
-				led4<= result(3);
-				led5<= fzero;
-				led6<= fsinal;
-				led7<= fover;
-				led8<= fcout;
-				--sempre ocorre
-				clock += "00000001";
-				parte = not parte;
+		case redutor is
+			when "‭1011111010111100000111111111‬"	=>
+			    redutor <= "0000000000000000000000000000";
+				case parte is
+				
+					when '0'=>
+					
+				--if parte 0
+						led1<= clock(0);
+						led2<= clock(1);
+						led3<= clock(2);
+						led4<= clock(3);
+						led5<= clock(4);
+						led6<= clock(5);
+						led7<= clock(6);
+						led8<= clock(7);
+					when '1'=>
+				--if parte 1
+						vetor1(0) <= clock(0);
+						vetor1(1) <= clock(1);
+						vetor1(2) <= clock(2);
+						vetor1(3) <= clock(3);
+						vetor2(0) <= clock(4);
+						vetor2(1) <= clock(5);
+						vetor2(2) <= clock(6);
+						vetor2(3) <= clock(7);
+						led1<= result(0);
+						led2<= result(1);
+						led3<= result(2);
+						led4<= result(3);
+						led5<= fzero;
+						led6<= fsinal;
+						led7<= fover;
+						led8<= fcout;
+						--sempre ocorre
+						clock += "00000001";
+						parte = not parte;
+					when others =>
+						parte = not parte;
+				end case;
 			when others =>
-				parte = not parte;
+				redutor += "0000000000000000000000000001";
 		end case;
 	end process bigode;
 
